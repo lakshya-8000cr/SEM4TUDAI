0d6c2085f29741b8a9f10c64394bb2497dbf9bcb
lakshya-8000cr <lakshyachauhan147@gmail.com>
2026-03-16 18:07:25 +0530

    adde dthe fib

---
+++ Recursion/pract3.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class pract3 {
+     static int fib(int n){
+        if(n==0 || n==1) return n;
+        return fib(n-1) + fib(n-2);
+     }
+
+     public static void main(String[] args){
+        for(int i=8; i>1; i--){
+            System.out.print(fib(i) + " ");
+        }
+     }
+}
