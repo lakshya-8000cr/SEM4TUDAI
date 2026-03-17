0a4cfd7586b3e08a6b712fdd1b04ca971f836e26
lakshya-8000cr <lakshyachauhan147@gmail.com>
2026-03-17 20:36:09 +0530

    added rec

---
+++ Recursion/pract5.java
@@ -0,0 +1,12 @@
+package Recursion;
+import java.util.*;
+public class pract5 {
+    public static  void fibo(int a , int b , int n){
+        if(n==0)return ;
+        fibo(b , a+b , n-1);
+        System.out.print(a + " ");
+    }
+    public static void main(String[] args){
+        fibo(0,1,7);
+    }
+}
